541daadffee59a06da56970dfd99ab514d84f333
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:11:57 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,4 @@
+// Q5. Famous Quotes 2: Repeat Exercise 4, but this time store the famous person's name in a variable called famous_person. Then compose your message and store it in a new variable called message.
+var famous_person = "Winston Churchill";
+var message = "Success is not final, failure is not fatal: It is the courage to continue that counts.";
+console.log("".concat(famous_person, " Once Said, \"").concat(message, "\""));
---
+++ Q5.ts
@@ -0,0 +1,7 @@
+// Q5. Famous Quotes 2: Repeat Exercise 4, but this time store the famous person's name in a variable called famous_person. Then compose your message and store it in a new variable called message.
+
+let famous_person: string = "Winston Churchill"
+
+let message: string = "Success is not final, failure is not fatal: It is the courage to continue that counts."
+
+console.log(`${famous_person} Once Said, "${message}"`);
